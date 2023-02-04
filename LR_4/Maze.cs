a4f19af967107e980a16bafc3a952d3f13ed3251
hibanari <87711726+hibanari@users.noreply.github.com>
2023-02-04 07:59:30 +0400

    Add files via upload

---
+++ LR_4/Maze.cs
@@ -0,0 +1,81 @@
+﻿class Maze
+{
+    //данные
+    int playerx = 1;
+    int playery = 1;
+    static public int count = 0;
+    int[,] maze = new int[,]
+    {
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+        {1, 0, 1, 0, 1, 0, 1, 0, 0, 1},
+        {1, 0, 1, 0, 1, 0, 0, 0, 1, 1},
+        {1, 0, 1, 0, 2, 1, 1, 0, 0, 1},
+        {1, 0, 0, 0, 1, 0, 1, 1, 0, 1},
+        {1, 1, 1, 0, 0, 0, 1, 0, 0, 1},
+        {1, 0, 1, 1, 0, 0, 1, 0, 1, 1},
+        {1, 0, 0, 0, 0, 1, 1, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+    };
+    ConsoleColor ink;
+    ConsoleColor paper;
+    public Maze(ConsoleColor i, ConsoleColor p)
+    {
+        ink = i;
+        paper = p;
+    }
+    //методы
+    public void MoveAndGetScore(int dx, int dy)
+    {
+        int nx = playerx + dx;
+        int ny = playery + dy;
+        if (maze[ny, nx] == 0 || maze[ny, nx] == 3)
+        {
+            playerx = nx;
+            playery = ny;
+            if (maze[ny, nx] == 0)
+            {
+                int m = 3;
+                maze[ny, nx] = m;
+                count++;
+            }
+        }
+        Action Exit = () => {
+            if (maze[ny, nx] == 2)
+            {
+                Console.SetCursorPosition(25, 12);
+                Console.WriteLine("Вы выйграли!");
+                System.Environment.Exit(0);
+            }
+        };
+        Exit();
+    }
+    public void Print(int shiftx, int shifty)
+    {
+        for (int y = 0; y < 10; y++)
+            for (int x = 0; x < 10; x++)
+            {
+                double dist = Math.Sqrt((playerx - x) * (playerx - x) + (playery - y) * (playery - y));
+                if (dist > 3.7)
+                {
+                    Print(shiftx + x, shifty + y, " ", ConsoleColor.Gray, ConsoleColor.DarkGray);
+                }
+                else
+                {
+                    if (maze[y, x] == 0) Print(shiftx + x, shifty + y, "*");
+                    else if (maze[y, x] == 2) Print(shiftx + x, shifty + y, "x");
+                    else if (maze[y, x] == 3) Print(shiftx + x, shifty + y, " ");
+                    else if (maze[y, x] == 1) Print(shiftx + x, shifty + y, "ツ", ink, paper);
+                }
+            }
+        Print(shiftx + playerx, shifty + playery, "?");
+    }
+    void Print(int x, int y, string s, ConsoleColor ink = ConsoleColor.Black, ConsoleColor paper = ConsoleColor.Cyan)
+    {
+        Console.ForegroundColor = ink;
+        Console.BackgroundColor = paper;
+        Console.CursorLeft = x;
+        Console.CursorTop = y;
+        Console.Write(s);
+    }
+}
---
+++ LR_4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+Console.Clear();
+Maze m = new Maze(ConsoleColor.DarkCyan, ConsoleColor.DarkGray);
+while (true)
+{
+    m.Print(3, 3);
+    ConsoleKeyInfo ki = Console.ReadKey(true);
+    if (ki.Key == ConsoleKey.LeftArrow) m.MoveAndGetScore(-1, 0);
+    if (ki.Key == ConsoleKey.RightArrow) m.MoveAndGetScore(1, 0);
+    if (ki.Key == ConsoleKey.UpArrow) m.MoveAndGetScore(0, -1);
+    if (ki.Key == ConsoleKey.DownArrow) m.MoveAndGetScore(0, 1);
+    Console.SetCursorPosition(25, 11);
+    Console.WriteLine($"Монетки = хорошо. Ваш счет: {Maze.count}.");
+}
